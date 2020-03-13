5721f9ea67007c6bf805381bc3a26919b3ae43e5
@telecristy <telematicacristina@gmail.com>
2020-03-12 22:47:42 -0700

    Adding project structure

---
+++ flexboxproject/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <nav>
+          <span>Cristina Rojas</span>
+          <ul>
+            <li>Option 1</li>
+            <li>Option 2</li>
+            <li>Option 3</li>
+            <li>Option 4</li>
+          </ul>
+        </nav>
+      </header>
+    </div>
+  );
+}
+
+export default App;
